d03888c1c45648435156924c69258176e67c6fde
synset <synnove.settman@yahoo.com>
2020-09-10 11:56:33 +0200

    Add project files.

---
+++ WebRequestGetExample/PostData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WebRequestGetExample
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            PostJson("https://localhost:44337/api/Speakers", new Speaker
+            {
+                name = "Will Smith",
+                Bio = "likes to drive cars",
+                webSite = "www.smith.com"
+            });
+        }
+
+
+        private static void PostJson(string uri, Speaker postParameters)
+        {
+            string postData = JsonConvert.SerializeObject(postParameters);
+            byte[] bytes = Encoding.UTF8.GetBytes(postData);
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+            httpWebRequest.Method = "POST";
+            httpWebRequest.ContentLength = bytes.Length;
+            httpWebRequest.ContentType = "application/json";
+            using (Stream requestStream = httpWebRequest.GetRequestStream())
+            {
+                requestStream.Write(bytes, 0, bytes.Count());
+            }
+            var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            Console.WriteLine(httpWebResponse);
+        }
+    }
+    public class Speaker
+    {
+        public string name { get; set; }
+        public string Bio { get; set; }
+        public string webSite { get; set; }
+    }
+}
